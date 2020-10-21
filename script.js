1ef306f34bfc0bf4d0f4955a49f06ddb58d31d68
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-10-21 17:13:48 -0400

    Fixed web app in accordance to feedback

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const searchURL = `https://developer.nps.gov/api/v1/parks?api_key=${api_key}`;
 let searchTerm = "";
 let stateCode = "";
 let states_list = "";
+let maxResults = 0;
 
-function displayResults(responseJson){
+function formatResults(responseJson){
     console.log(responseJson);
-    let html = "";
+    let html_to_display = '';
     $('#results-list').empty();
     if (responseJson.data.length != 0){
         for (let i = 0;i <responseJson.data.length;i++){
@@ -17,7 +18,6 @@ function displayResults(responseJson){
             let mailingAddress = "";
             for (j = 0; j < addresses.length; j++){
                 let address = `${addresses[j].line1}\n${addresses[j].line2}\n${addresses[j].line3}<br><br>${addresses[j].city} ${addresses[j].stateCode} ${addresses[j].postalCode}`;
-                console.log(address)
                 if (addresses[j].type == "Physical"){
                     physicalAddress = address;
                 }else{
@@ -25,22 +25,18 @@ function displayResults(responseJson){
                 }
             }
     
-            html += `<li><h3>${responseJson.data[i].fullName}</h3><p>${responseJson.data[i].description}</p><a href = "${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a><br></br><h4>See it in person</h4><p>${physicalAddress}</p><h4>Send them a letter</h4><p>${mailingAddress}</p></li>`
+            html_to_display += `<li><h3>${responseJson.data[i].fullName}</h3><p>${responseJson.data[i].description}</p><a href = "${responseJson.data[i].url}" target="_blank">${responseJson.data[i].url}</a><br></br><h4>See it in person</h4><p>${physicalAddress}</p><h4>Send them a letter</h4><p>${mailingAddress}</p></li>`
         }
     }else{
-        html = `<li>There are no parks named <strong>${searchTerm}</strong> in <strong>${stateCode}</strong></li>`
-    }
-        
-    $('#results-list').append(html);
+        html_to_display += `<li>There are no parks named <strong>${searchTerm}</strong> in <strong>${stateCode}</strong></li>`
+    } 
+    $('#results-list').append(html_to_display);
     $('#results').removeClass('hidden');
   }
 
-
 function getPark(stateCode,limit){
-
     const url = `${searchURL}&stateCode=${stateCode}&limit=${limit}&start=0`;
     console.log(url);
-    
     fetch(url)
     .then(response => {
         if (response.ok) {
@@ -48,29 +44,30 @@ function getPark(stateCode,limit){
         }
         throw new Error(response.statusText);
     })
-    .then(responseJson => displayResults(responseJson))
+    .then(responseJson => formatResults(responseJson))
     .catch(err => {
         $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
+}   
 
-function getParks(states_list){
-    for (let i = 0; i <states_list.length; i++){
-        let stateCode =""
-        
-    }
-}
+
+// function getParks(states_list){
+//     $('#results-list').empty();
+//     for (let i = 0; i <states_list.length; i++){
+//         stateCode = states_list[i];
+//         console.log(stateCode)
+//         getPark(stateCode, maxResults);
+//     }
+//     $('#results').removeClass('hidden');
+// }
 
 function watchForm() {
     $('form').submit(event => {
       event.preventDefault();
-    //   searchTerm = $('#search-term').val();
-    //   stateCode = $('#state').val();
       const states = $('#list-of-states').val();
       states_list = (states.trim().split(','))
-      console.log(states_list)
-      const maxResults = $('#number-of-results').val();
-    // getPark(searchTerm,stateCode,maxResults);
+      maxResults = $('#number-of-results').val();
+      getPark(states_list,maxResults);
     });
 }
 
